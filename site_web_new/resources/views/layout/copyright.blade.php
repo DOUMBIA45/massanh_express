693205252ddf725c752d305c9b72a731f1afad33
DOUMBIA45 <adama.doumbia@uvci.edu.ci>
2024-03-03 03:42:56 +0100

    deleted old version

--- site_web_new/resources/views/layout/copyright.blade.php
+++
@@ -1,8 +0,0 @@
-
-<div class="row">
-    <div class="col-md-3 text-center"></div>
-    <div class="col-md-6 text-center">
-         <a href="/">MASSANANH EXPERTIZ</a>&copy; all Right Reserved.
-    </div>
-    <div class="col-md-3 text-center"></div>
-</div>
